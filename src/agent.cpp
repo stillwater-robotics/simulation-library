9f761f3c6232ac6e6c8f3843d7cd50fa0726c34e
svillemure <svillemure@yhaoo.ca>
2025-12-29 11:01:49 -0500

    compiles

--- src/agent.cpp
+++ src/agent.cpp
@@ -1,19 +1,65 @@
 #include "agent.h"
 
-Agent::Agent(State initialState){
-    // TODO expose
-    pointGenerator = &PointGenerator(1.0f, 1.0f, 1.0f);
-    trajectoryGenerator = &TrajectoryGenerator();
-    controller = &Controller();
+#include <sstream>
 
+#include <iostream>  // TODO REMOVE
+
+Agent::Agent(float id_, State initialState, uint32_t sim_start_, float speed, float dive_time){
+    // TODO expose
+    pointGenerator = PointGenerator(1.0f, 1.0f, 1.0f);
+    trajectoryGenerator = TrajectoryGenerator(speed, dive_time);
+    controller = Controller();
+
+    id = id_;
     currentState = initialState;
+    time = 0;
+    traj_time = 0;
+    desired = currentState.pose;
+
+    sim_start = sim_start_;
+    std::stringstream state_filename;
+    state_filename << "sim_data_" << sim_start << "_" << id << "_states.csv";
+    state_writer = Writer(state_filename.str());
+
+    std::stringstream desired_filename;
+    desired_filename << "sim_data_" << sim_start << "_" << id << "_desired_poses.csv";
+    std::cout << desired_filename.str() << std::endl;
+    desired_state_writer = Writer(desired_filename.str());
 }
 
-void Agent::Update(std::vector<State> states){
-    //TODO
+void Agent::Plan(std::vector<State> states){
+    std::vector<Pose> poses;
+    for(auto it=states.begin(); it < states.end(); ++it){
+        poses.push_back((*it).pose);
+    }
+    desired = pointGenerator.Update(currentState.pose, poses);
+    trajectoryGenerator.Update(currentState.pose, desired);
+
+    traj_time = time;
+    return;
+}
+
+void Agent::Update(float time_step){
+    controller.Update(trajectoryGenerator.GetDesiredState(time-traj_time));
+    time += time_step;
     return;
 }
 
 State Agent::ReadState(){
+    // TODO add simulated state estimation error
     return currentState;
+}
+
+void Agent::WriteState(){
+    state_writer.Write(ReadState(), time);
+}
+
+void Agent::WriteDesiredState(){
+    desired_state_writer.Write(desired, time);
+}
+
+void Agent::WriteDesiredTrajectory(){
+    std::stringstream filename;
+    filename << "sim_data_" << sim_start << "_" << id << "_trajectory_" << traj_time << ".csv";
+    WriteTrajectory(trajectoryGenerator.trajectory, filename.str(), 0.1);
 }
---
+++ src/all_states_known.cpp
@@ -0,0 +1,61 @@
+#include "agent.h"
+
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+#define NUM_AGENTS 4
+#define SPEED 1
+#define DIVE_TIME 5
+
+#define SIM_TIME 3
+#define CONTROL_TIME_STEP 0.1
+#define PLAN_TIME_STEP 10*CONTROL_TIME_STEP
+
+State get_start_state(int id){
+    return State(Pose(id, id, 0, 0), Pose(0, 0, 0, 0));
+}
+
+void replan_all(std::vector<Agent> agents){
+    for (auto it=agents.begin(); it < agents.end(); ++it){
+        std::vector<State> states;
+        for (auto it2=agents.begin(); it2 < agents.end(); ++it2){
+            if (it->id == it2->id){
+                continue;
+            }
+            states.push_back(it2->ReadState());
+        }
+        it->Plan(states);
+        it->WriteDesiredState();
+        it->WriteDesiredTrajectory();
+        std::cout << "wrote replan to files" << std::endl;
+    }
+}
+
+void update_all(std::vector<Agent> agents){
+    for (auto it=agents.begin(); it < agents.end(); ++it){
+        it->Update(CONTROL_TIME_STEP);
+        it->WriteState();
+        std::cout << "wrote update to files" << std::endl;
+    }
+}
+
+int main() {
+    // Initilize the simulation
+    uint32_t sim_start = std::chrono::duration_cast<std::chrono::seconds>(
+                   std::chrono::system_clock::now().time_since_epoch()
+                   ).count();
+    
+    std::vector<Agent> agents;
+    for (int i=1; i<=NUM_AGENTS; i++){
+        agents.push_back(Agent(i, get_start_state(i), sim_start, SPEED, DIVE_TIME));
+    }
+    
+    for (float i=0; i<=SIM_TIME; i+=CONTROL_TIME_STEP){
+        std::cout << "Simulation time: " << i << std::endl;
+        if (fmod(i, PLAN_TIME_STEP) < CONTROL_TIME_STEP){
+            replan_all(agents);
+        }
+        update_all(agents);
+    }
+}
